7093fee4a56fddf799112288a10b6aafa1fd820c
Saidazam Abdukhalilov <maxi123442234@gmail.com>
2026-05-29 14:25:22 +0500

    Add AI task: Talaba baho baholash dasturi

---
+++ ai_topshiriqlar/Talaba_baho_baholash_dasturi.py
@@ -0,0 +1,17 @@
+# Talaba baho baholash dasturi
+# Kurs: IT Dasturlash
+# Mavzu: if, elif, else asoslari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+b = int(input())
+if b >= 90:
+    print("A")
+elif b >= 80:
+    print("B")
+elif b >= 70:
+    print("C")
+elif b >= 60:
+    print("D")
+else:
+    print("F")
